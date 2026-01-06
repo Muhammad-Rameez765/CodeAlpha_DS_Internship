048a66d66d208f59d77f040a68f5cd17cae88e18
Muhammad Rameez <ramezianramos@gmail.com>
2026-01-06 20:30:51 +0500

    Add files via upload

---
+++ CodeAlpha_Iris_Flower_Classification/src/model_evaluation.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+    cm = confusion_matrix(y_test, y_pred)
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+    plt.show()
